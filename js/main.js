bf7b3b4d175ef679341d1a7348117e7ad1f89077
PREM <prem92006@gmail.com>
2026-01-17 19:57:14 +0530

    Update main.js

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function loadComponent(id, file) {
+  fetch(file)
+    .then(res => res.text())
+    .then(data => document.getElementById(id).innerHTML = data)
+}
+
+loadComponent("header", "components/header.html")
+loadComponent("footer", "components/footer.html")
+loadComponent("cards", "components/card.html")
